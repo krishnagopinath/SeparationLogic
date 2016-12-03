d1b84785ee512d22a0248e4f21017e468c23f71d
krishna gopinath <hpkrishna07@gmail.com>
2016-12-02 21:58:52 -0500

    Added remove, lookup, split, disjoint methods to the partial_maps

--- Maps.v
+++ Maps.v
@@ -1,5 +1,7 @@
 (** * Maps: Total and Partial Maps *)
 
+(* TODO : Reprove some partial_map Lemmas *) 
+
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
 Require Import Coq.Logic.FunctionalExtensionality.
@@ -62,17 +64,13 @@ Definition t_update {A:Type} (m : total_map A)
                     (x : id) (v : A) :=
   fun x' => if beq_id x x' then v else m x'.
 
+Definition t_remove {A:Type} (m : total_map A) (def : A) (key : id) : total_map A :=
+  fun key' => if beq_id key key' then def else m key'.
 
 Lemma t_update_eq : forall A (m: total_map A) x v,
   (t_update m x v) x = v.
 Proof.
   intros. unfold t_update. rewrite <-beq_id_refl. reflexivity. Qed.
-(** [] *)
-
-(** **** Exercise: 2 stars, optional (t_update_neq)  *)
-(** On the other hand, if we update a map [m] at a key [x1] and then
-    look up a _different_ key [x2] in the resulting map, we get the
-    same result that [m] would have given: *)
 
 Theorem t_update_neq : forall (X:Type) v x1 x2
                          (m : total_map X),
@@ -100,54 +98,58 @@ Theorem t_update_permute : forall (X:Type) v1 v2 x1 x2
   = (t_update (t_update m x1 v1) x2 v2).
 Proof. Admitted.
 
+Lemma t_apply_empty:  forall A x v, @t_empty A v x = v.
+Proof. Admitted.
+
 (* ################################################################# *)
 (** * Partial maps *)
 
 Definition partial_map (A:Type) := total_map (option A).
 
 Definition empty {A:Type} : partial_map A :=
-  t_empty None.
+  fun _ => None. 
 
 Definition update {A:Type} (m : partial_map A)
-                  (x : id) (v : A) :=
-  t_update m x (Some v).
+                  (key : id) (v : A) :=
+  fun key' => if beq_id key key' then Some v else m key'.
+
+Definition lookup {A:Type} (m : partial_map A)
+           (key : id) := m key.
 
 Definition remove {A:Type} (m : partial_map A) (key : id) : partial_map A :=
   fun key' => if beq_id key key' then None else m key'.
-  
-Lemma t_apply_empty:  forall A x v, @t_empty A v x = v.
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
+
+Definition merge {A:Type} (m1 m2 : partial_map A) : partial_map A :=
+  fun key => match (m1 key) with
+             | None => m2 key
+             | v => v
+             end.
+
+Definition split {A:Type} (m m1 m2 : partial_map A) : Prop :=
+  (* Using an Inductive defn was an overkill for this, I think *) 
+  m = merge m1 m2.
+
+Definition disjoint {A: Type} (m1 m2 : partial_map A) : Prop :=
+  (* dom(m1) and dom(m2) = null. *) 
+  (* If m1 and m2 give out values for the same Id, then the heaps are NOT disjoint *)
+  forall (a : id),  (m1 a <> None) /\ (m2 a <> None) -> False. 
 
 Lemma apply_empty : forall A x, @empty A x = None.
-Proof.
-  intros. unfold empty. rewrite t_apply_empty.
-  reflexivity.
-Qed.
+Proof. Admitted.
 
 Lemma update_eq : forall A (m: partial_map A) x v,
   (update m x v) x = Some v.
-Proof.
-  intros. unfold update. rewrite t_update_eq.
-  reflexivity.
-Qed.
+Proof. Admitted.
 
 Theorem update_neq : forall (X:Type) v x1 x2
                        (m : partial_map X),
   x2 <> x1 ->
   (update m x2 v) x1 = m x1.
-Proof.
-  intros X v x1 x2 m H.
-  unfold update. rewrite t_update_neq. reflexivity.
-  apply H. Qed.
+Proof. Admitted.
 
 Lemma update_shadow : forall A (m: partial_map A) v1 v2 x,
   update (update m x v1) x v2 = update m x v2.
-Proof.
-  intros A m v1 v2 x1. unfold update. rewrite t_update_shadow.
-  reflexivity.
-Qed.
+Proof. Admitted.
 
 Theorem update_same : forall X v x (m : partial_map X),
   m x = Some v ->
